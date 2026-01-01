36911bdd42500a60cb013bcd934179b7944e202d
Steven Parker <stevepetebruce@hotmail.com>
2026-01-01 20:06:24 +0000

    Add react suspense
    Add react suspense to the main app page for loading state.

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,3 +1,12 @@
+import { Loader2Icon } from "lucide-react";
+import { Suspense } from "react";
+
 export default function ApPage() {
-    return <div>Welcome to Learn Spanish!</div>;
+    return (<Suspense fallback={ <div className="h-screen-header flex items-center justify-center">
+            <Loader2Icon className="size-24 animate-spin" />
+        </div>}>
+        <div>
+           Welcome to Learn Spanish!
+        </div>
+    </Suspense>);
 }
