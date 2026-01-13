f0de57a575d57ba927e949ce202fe927e829d120
Steven Parker <stevepetebruce@hotmail.com>
2026-01-13 22:27:21 +0000

    Add Language description cards
    Add Language description cards to app page

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,13 +1,18 @@
 import { db } from "@/drizzle/db";
 import { getCurrentUser } from "@/services/clerk/lib/getCurrentUser"
 import { desc, eq } from "drizzle-orm";
-import { Loader2Icon } from "lucide-react";
+import { ArrowRightIcon, Loader2Icon, PlusIcon } from "lucide-react";
 import { LanguageInfoTable } from "@/drizzle/schema";
 import { Suspense } from "react";
 import { cacheTag } from "next/dist/server/use-cache/cache-tag";
 import { getLanguageInfoUserTag } from "@/features/languageInfos/dbCache";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
 import { LanguageInfoForm } from "@/features/languageInfos/components/LanguageInfoForm";
+import { Button } from "@/components/ui/button"
+import Link from "next/link";
+import { Arrow } from "@radix-ui/react-dropdown-menu";
+import { formatExperienceLevel } from "@/features/languageInfos/lib/formatters";
 
 export default function ApPage() {
     return (<Suspense fallback={ <div className="h-screen-header flex items-center justify-center">
@@ -27,7 +32,50 @@ async function LanguageInfos() {
         return <NoLanguageInfos />;
     }
 
-    return null;
+    return (
+        <div className="container my-4">
+            <div className="flex gap-2 justify-between mb-6">
+                <h1 className="text-3xl md:text-4xl lg:text-5xl">
+                Select a language description
+                </h1>
+                <Button asChild>
+                <Link href="/app/language-infos/new">
+                    <PlusIcon />
+                    Create Language Description
+                </Link>
+                </Button>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+                {languageInfos.map((languageInfo) => (
+                    <Link
+                        key={languageInfo.id}
+                        href={`/app/language-infos/${languageInfo.id}`}
+                    >
+                        <Card className="h-full">
+                           <div className="flex items-center justify-between h-full">
+                                <div className="space-y-4 h-full">
+                                    <CardHeader>
+                                        <CardTitle className="text-lg">{languageInfo.name}</CardTitle>
+                                    </CardHeader>
+                                    <CardContent className="text-muted-foreground line-clamp-3">
+                                        {languageInfo.description}
+                                    </CardContent>
+                                    
+                                <CardFooter className="flex gap-2">
+                                    <Badge variant="outline">{formatExperienceLevel(languageInfo.experienceLevel)}</Badge>
+                                    {languageInfo.title && (<Badge variant="outline">{languageInfo.title}</Badge>)}
+                                </CardFooter>
+                                </div>
+                                <CardContent>
+                                        <ArrowRightIcon className="size-6" />
+                                </CardContent>
+                            </div>
+                        </Card>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 function NoLanguageInfos() {
